89e8bd52def69f4dd737554ea9d8c8258d810d03
mutahir123mutahir123 <mutahirhussain03@gmail.com>
2026-04-14 01:54:03 +0500

    Fix: Change base to relative path for deployment

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
-  publicDir: 'public',
+  base: './',
   build: {
     target: 'esnext',
     minify: 'esbuild',
+    assetsInlineLimit: 0,
     rollupOptions: {
       output: {
         manualChunks: {
